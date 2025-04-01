37ac4e9df91e61bdb742849ea2f832396086f124
psingh-conest <psingh6518@conestogac.on.ca>
2025-04-01 19:11:08 +0000

    Schema validation and migrations

--- dbtests.py
+++ dbtests.py
@@ -21,5 +21,10 @@ columns = {col[0] for col in cursor.fetchall()}
 required_columns = {"id", "email", "subscription_date"}
 assert required_columns.issubset(columns), f"Missing columns: {required_columns - columns}"
 
+# verify inserted data
+cursor.execute("SELECT subscription_date FROM subscriber LIMIT 1")
+assert cursor.fetchone(), "Subscription date column is missing data!"
+
+
 print(" Schema validation passed!")
 conn.close()
